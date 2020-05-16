44acde7d92dfa1fc3f56ad2a728d8a759927bec1
vinojglm2965 <lorantmarquina>
2020-05-16 17:06:13 -0400

    arreglo

--- sas.js
+++ sas.js
@@ -1,12 +1,14 @@
-var one = ['rojo', 'azul', 'amarillo,'];
-var two = ['verde', 'naranja', 'morado'];
-var three = ['blanco', 'negro', 'gris'];
+var one = ['rojo', 'azul', 'amarillo',];
+var two = ['verde', 'naranja', 'morado',];
+var three = ['blanco', 'negro', 'gris',];
 
     function mezcla (){
-        for(let i=0; i<3; i++){
-            console.log('www.' + one[i] + two[i] + three[i] + '.com');
-            console.log('www.' + three[i] + one[i] + two[i] + '.com');
-            console.log('www.' +two[i] + three[i] + one[i] + '.com');
+        for (let i=0; i<3; i++){
+            for(let j=0; j<3; j++){
+                for(let x=0; x<3; x++){
+                    console.log('www.' + one[i] + two[j] + three[x] + '.com');
+                }
+            }
         }
     }
 mezcla();
